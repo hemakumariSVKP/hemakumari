db4c525f43a48f3b0901ff7fcfe111a9f1835209
ACER <ACER@192.168.1.41>
2024-10-24 16:53:35 +0530

    second  commit

---
+++ Svkp8/src/sample/whileDemo.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class whileDemo {
+	public static void main(String[] args) {
+		int Number=1;
+		while(Number<10) {
+			System.out.println("hema kumari"+" "+Number);
+			Number++;
+		}
+
+	}
+
+}
