c0897a5a1bf60e6f04c30f87257034d7c386136c
ACER <ACER@192.168.1.41>
2024-10-25 15:53:13 +0530

    Third commit

---
+++ Svkp8/src/sample/StringDemo.java
@@ -0,0 +1,15 @@
+package sample;
+
+ class  Demo{
+	String strvalue;
+	int a[];
+  }
+ //DefaultValueChecking
+  public class  StringDemo{
+	  
+	public static void main(String[] args) {
+		Demo d1= new Demo();
+		System.out.println(d1.strvalue);
+		System.out.println(d1.a);
+	}
+ }
